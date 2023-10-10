06c2d00502bcee89feb48cd188789dade1cc70c7
koitabashi0222 <koita0222yuh@icloud.com>
2023-10-10 17:03:10 +0900

    10/10進捗
    視界判定できた
    判定は[https://nekojara.city/unity-object-sight]をそのまま
    RayCastで障害物の判定、視界の外に行っても一定時間(可変)プレイヤーの方を見続け、超至近距離に行った時は視界の外でも振り返ってくるようにしてます(これはいらないかも)

--- Fovtest/Assets/SightChecker.cs
+++ Fovtest/Assets/SightChecker.cs
@@ -2,23 +2,72 @@ using UnityEngine;
 
 public class SightChecker : MonoBehaviour
 {
-    // 自分自身
     [SerializeField] private Transform _self;
-
-    // ターゲット
     [SerializeField] private Transform _target;
-
-    // 視野角（度数法）
     [SerializeField] private float _sightAngle;
-
-    // 視界の最大距離
     [SerializeField] private float _maxDistance = float.PositiveInfinity;
+    [SerializeField] private float _rotationSpeed = 5.0f;
 
+    private bool isPlayerInSight = false;
+    private float timeSincePlayerLost = 0.0f;
+    [SerializeField] private float timeToKeepFacingPlayer = 1.0f; // プレイヤーの方を向き続ける時間
+    [SerializeField] private float _minDistanceToReact = 2.0f; // 至近距離で反応する最小距離
+    [SerializeField] private LayerMask _obstacleLayer;
+
+    private void Update()
+    {
+        float distanceToPlayer = Vector3.Distance(_self.position, _target.position);
+
+        if (distanceToPlayer <= _minDistanceToReact)
+        {
+            // プレイヤーが至近距離にいる場合、ターゲットの方を向く
+            Vector3 targetDirection = (_target.position - _self.position).normalized;
+            // レイキャストを使用して障害物がないか確認
+            if (!IsObstacleBetween(_self.position, _target.position))
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+                _self.rotation = Quaternion.Slerp(_self.rotation, targetRotation, Time.deltaTime * _rotationSpeed);
+            }
+        }
+        else
+        {
+            // プレイヤーが至近距離にいない場合、視界判定を行う
+            bool isVisible = IsVisible();
+            timeSincePlayerLost += Time.deltaTime;
+            if (isVisible)
+            {
+                // ターゲットが見えている場合、視界内の通常の処理を行う
+                // レイキャストを使用して障害物がないか確認
+                if (!IsObstacleBetween(_self.position, _target.position))
+                {
+                    timeSincePlayerLost = 0.0f;
+                    isPlayerInSight = true;
+                    Vector3 targetDirection = (_target.position - _self.position).normalized;
+                    Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+                    _self.rotation = Quaternion.Slerp(_self.rotation, targetRotation, Time.deltaTime * _rotationSpeed);
+                }
+            }
+            else if (isPlayerInSight&& !IsObstacleBetween(_self.position, _target.position))
+            {
+                // ターゲットが見えていないが、最初の何秒間かはプレイヤーの方を向き続ける
+                if (timeSincePlayerLost <= timeToKeepFacingPlayer)
+                {
+                    Vector3 playerDirection = (_target.position - _self.position).normalized;
+                    Quaternion playerRotation = Quaternion.LookRotation(playerDirection);
+                    _self.rotation = Quaternion.Slerp(_self.rotation, playerRotation, Time.deltaTime * _rotationSpeed);
+                }
+                else
+                {
+                    isPlayerInSight = false;
+                }
+            }
+            else
+            {
+                // ターゲットが見えていない場合、何もしないか、別の行動を追加する
+            }
+        }
+    }
     #region Logic
-
-    /// <summary>
-    /// ターゲットが見えているかどうか
-    /// </summary>
     public bool IsVisible()
     {
         // 自身の位置
@@ -44,10 +93,24 @@ public class SightChecker : MonoBehaviour
         return innerProduct > cosHalf && targetDistance < _maxDistance;
     }
 
+    private bool IsObstacleBetween(Vector3 start, Vector3 end)
+    {
+        // レイキャストを使用して対象との間に障害物があるか確認
+        Vector3 direction = end - start;
+        float distance = direction.magnitude;
+        if (Physics.Raycast(start, direction, out RaycastHit hit, distance, _obstacleLayer))
+        {
+            // 障害物がある場合は true を返す
+            return true;
+        }
+
+        // 障害物がない場合は false を返す
+        return false;
+    }
     #endregion
     private void OnDrawGizmos()
     {
-        Gizmos.color = Color.red; // カラーを設定（赤色など）
+        Gizmos.color = Color.green; // カラーを設定（赤色など）
         Gizmos.DrawWireSphere(transform.position, _maxDistance); // 最大距離の円を描画
         Gizmos.DrawLine(transform.position, _target.position); // 自身からターゲットへのラインを描画
         Gizmos.matrix = Matrix4x4.TRS(transform.position, Quaternion.LookRotation(_self.forward), Vector3.one);
