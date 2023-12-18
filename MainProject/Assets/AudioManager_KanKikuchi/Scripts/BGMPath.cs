99e50bbc08f16abd153f30f7d284b606e4501944
MettoNao <nt7.00.harry@gmail.com>
2023-12-18 12:57:48 +0900

    BGM追加

--- MainProject/Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
+++ MainProject/Assets/AudioManager_KanKikuchi/Scripts/BGMPath.cs
@@ -5,6 +5,7 @@
 /// </summary>
 public static class BGMPath{
 
+	public const string MUS_MUS_BGM104 = "BGM/MusMus-BGM-104";
 
 }
 
--- MainProject/Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
+++ MainProject/Assets/AudioManager_KanKikuchi/Scripts/SEPath.cs
@@ -5,9 +5,11 @@
 /// </summary>
 public static class SEPath{
 
-	public const string BUTTON   = "SE/Button";
-	public const string KILL     = "SE/Kill";
-	public const string MAP_DOWN = "SE/Map_Down";
+	public const string BUTTON    = "SE/Button";
+	public const string HIGHLIGHT = "SE/Highlight";
+	public const string KILL      = "SE/Kill";
+	public const string ROPE      = "SE/rope";
+	public const string WIND      = "SE/wind";
 
 }
 
